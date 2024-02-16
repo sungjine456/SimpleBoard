ea01b41b9852b99b0a548f6846c75a6e0c222ff5
sungjine456 <sungjine456@gmail.com>
2024-02-16 16:50:06 +0900

    테스트 데이터를 더 명확하게 하기 위한 수정

--- backend/src/test/java/com/example/Board/DatabaseCleanUp.java
+++ backend/src/test/java/com/example/Board/DatabaseCleanUp.java
@@ -36,6 +36,8 @@ public class DatabaseCleanUp implements InitializingBean {
 
         for (String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN id RESTART WITH 1")
+                    .executeUpdate();
         }
 
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
