74132438215641963afacc4d144fd1f2f8506938
sungjine456 <sungjine456@gmail.com>
2024-02-24 18:47:28 +0900

    메인 페이지 버튼 구조 변경

--- frontend/src/components/forms/SignInFormComponent.tsx
+++ frontend/src/components/forms/SignInFormComponent.tsx
@@ -1,10 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import MemberToEmailRequest from "../../models/requests/MemberToEmailRequest";
 import { useSignIn } from "../../services/MemberService";
 
 function SignInFormComponent() {
-  const navigate = useNavigate();
   const signIn = useSignIn();
 
   const {
@@ -23,61 +21,58 @@ function SignInFormComponent() {
   };
 
   return (
-    <form>
-      <div>
-        <label>이메일</label>
-        <input
-          type="email"
-          placeholder="이메일"
-          aria-invalid={
-            isSubmitted ? (errors.email ? "true" : "false") : undefined
-          }
-          {...register("email", {
-            required: "이메일을 입력해주세요.",
-            pattern: {
-              value:
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        ></input>
-      </div>
-      {errors.email && <small role="alert">{errors.email.message}</small>}
-      <div>
-        <label>비밀번호</label>
-        <input
-          type="password"
-          placeholder="비밀번호"
-          aria-invalid={
-            isSubmitted ? (errors.password ? "true" : "false") : undefined
-          }
-          {...register("password", {
-            required: "비밀번호를 입력해주세요.",
-            minLength: {
-              value: 8,
-              message: "8자리 이상 비밀번호를 사용하세요.",
-            },
-          })}
-        ></input>
-      </div>
-      {errors.password && <small role="alert">{errors.password.message}</small>}
-      <div className="buttons">
-        <button
-          className="btn btn-light"
-          type="submit"
-          onClick={handleSubmit(onSubmit)}
-        >
-          로그인
-        </button>
-        <button
-          className="btn btn-light"
-          type="button"
-          onClick={() => navigate("/signUp")}
-        >
-          가입하기
-        </button>
-      </div>
-    </form>
+    <div className="d-flex">
+      <form id="sign-in-form">
+        <div>
+          <label>이메일</label>
+          <input
+            type="email"
+            placeholder="이메일"
+            aria-invalid={
+              isSubmitted ? (errors.email ? "true" : "false") : undefined
+            }
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value:
+                  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
+            })}
+          ></input>
+        </div>
+        {errors.email && <small role="alert">{errors.email.message}</small>}
+        <div>
+          <label>비밀번호</label>
+          <input
+            type="password"
+            placeholder="비밀번호"
+            aria-invalid={
+              isSubmitted ? (errors.password ? "true" : "false") : undefined
+            }
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              minLength: {
+                value: 8,
+                message: "8자리 이상 비밀번호를 사용하세요.",
+              },
+            })}
+          ></input>
+        </div>
+        {errors.password && (
+          <small role="alert">{errors.password.message}</small>
+        )}
+      </form>
+      <button
+        className="btn btn-light"
+        type="submit"
+        style={{ marginLeft: "12px" }}
+        form="sign-in-form"
+        onClick={handleSubmit(onSubmit)}
+      >
+        로그인
+      </button>
+    </div>
   );
 }
 
--- frontend/src/components/pages/MainPageComponent.tsx
+++ frontend/src/components/pages/MainPageComponent.tsx
@@ -2,14 +2,25 @@ import { useContext } from "react";
 import SignOutButton from "../buttons/SignOutButton";
 import { AuthContext } from "../contexts/AuthContext";
 import SignInFormComponent from "../forms/SignInFormComponent";
+import { useNavigate } from "react-router-dom";
 
 function MainPageComponent() {
   const { authenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   return (
-    <div>
-      <p>{authenticated}</p>
+    <div className="d-flex-column">
       {authenticated ? <SignOutButton /> : <SignInFormComponent />}
+
+      <div className="buttons align-self-end">
+        <button
+          className="btn btn-light"
+          type="button"
+          onClick={() => navigate("/signUp")}
+        >
+          가입하기
+        </button>
+      </div>
     </div>
   );
 }
