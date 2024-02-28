78139ac5d98d03a1289b9314a230527c2ce6f65c
sungjine456 <sungjine456@gmail.com>
2024-02-28 20:43:38 +0900

    불필요한 코드 제거

--- backend/src/test/java/com/example/Board/utils/CommonsTest.java
+++ backend/src/test/java/com/example/Board/utils/CommonsTest.java
@@ -4,9 +4,7 @@ import static com.example.Board.utils.Commons.isEmailFormat;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CommonsTest {
 
     @Test
